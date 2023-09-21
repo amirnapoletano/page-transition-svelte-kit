170f3606eb0ba579a09b52598d0ac677475fa9b9
Amir Napoletano <amirnapoletano@gmail.com>
2023-09-21 16:41:25 +0200

    first attempt

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { fly } from 'svelte/transition'
+    import PageTransition from '$lib/transition.svelte'
+    export let data
+</script>
+
+{#key data.url}
+    <div
+            in:fly={{ x: -200, duration: 300, delay: 300 }}
+            out:fly={{ x: 200, duration: 300 }}>
+        <PageTransition key={data.url} duration={2000}>
+        <slot />
+            </PageTransition>
+    </div>
+{/key}
